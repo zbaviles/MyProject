c17a7bdc714767de0cef93bb751b528cb6827969
Everardo Aviles <170151733+zbaviles@users.noreply.github.com>
2025-05-31 18:05:17 -0700

    Create zoombushing.cpp

---
+++ test/zoombushing.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <dos.h>
+void main()
+{
+clrscr();
+int 1;
+textcolor(RED);
+textbackground(WHITE);
+for(i=1;i<=120;i++)
+{
+cprintf(”  I LOVE YOU \j  “);
+delay(200);
+}
+getch();
+}
